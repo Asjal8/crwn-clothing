27e3f58d42ae6632f1c66c7afd0540462c9adadd
Asjal Abrar <asjalabrar8@gmail.com>
2022-06-11 22:07:27 +0500

    broken our homepage into smaller components and style the menu-iem

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Directory from '../../components/directory/Directory';
+
+import './Homepage.scss';
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+    <Directory />
+  </div>
+  )
+}
